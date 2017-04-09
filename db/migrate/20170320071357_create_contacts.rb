d5e7561d01b4d9dabb1173f560dbbde435a5f50d
Tsuyoshi Yasui <tyasui@nttpc.co.jp>
2017-04-09 12:23:07 +0000

    first commit

---
+++ db/migrate/20170320071357_create_contacts.rb
@@ -0,0 +1,13 @@
+class CreateContacts < ActiveRecord::Migration
+  def change
+    create_table :contacts do |t|
+
+      t.text :name
+      t.text :email
+      t.text :content
+
+
+      t.timestamps null: false
+    end
+  end
+end
