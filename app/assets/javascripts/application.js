33830200c74ac04b5c11be7adadb0f18fdca7a1a
Tsuyoshi Yasui <tyasui@nttpc.co.jp>
2017-09-14 04:39:47 +0000

    init unicorn

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery.turbolinks
+//= require twitter/bootstrap
+//= require turbolinks
 //= require_tree .
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-server '13.113.232.4', user: 'app', roles: %w{app db web}
+server '13.113.232.44', user: 'app', roles: %w{app db web}
 set :ssh_options, keys: '/home/ubuntu/.ssh/id_rsa'
